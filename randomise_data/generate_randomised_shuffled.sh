4dc0d2cb83ecfac7e5038f19f71b97c20b728b53
Gibran Hemani <explodecomputer@gmail.com>
2014-11-20 16:01:14 +0000

    modified genetic data shuffling to account for correct sex
    shuffling the x chromosome IDs lead sto permutation of sex status
    modified script to only shuffle autosomes, and performed sex check in plink

--- randomise_data/generate_randomised_shuffled.sh
+++ randomise_data/generate_randomised_shuffled.sh
@@ -5,12 +5,15 @@ cd ~/repo/sa_course/randomise_data
 
 # Extract unrelated kids from imputed data
 # Permute fam file rows for each chromosome
-for i in {1..23}
+for i in {1..22}
 do
 	plink1.90 --bfile ~/1kg_scratch/chr${i}/alspac_1kg_p1v3_${i}_maf0.01_info0.8 --keep ~/1kg_scratch/docs/Unrelateds/CHILDREN_UNRELATED_PLINK.txt --make-bed --out scratch/chr${i}
 	cp scratch/chr${i}.fam scratch/chr${i}.fam.orig
 	cat scratch/chr${i}.fam.orig | shuf > scratch/chr${i}.fam
 done
+
+# Don't shuffle X chromosome - sex should relate to genotype
+plink1.90 --bfile ~/1kg_scratch/chr23/alspac_1kg_p1v3_23_maf0.01_info0.8 --keep ~/1kg_scratch/docs/Unrelateds/CHILDREN_UNRELATED_PLINK.txt --make-bed --out scratch/chr23
 
 # Create merge file
 echo "scratch/chr2.bed scratch/chr2.bim scratch/chr2.fam" > mergefile.txt
@@ -20,8 +23,11 @@ do
 done
 
 # Merge chromosomes into one file
-plink1.90 --bfile scratch/chr1 --merge-list mergefile.txt --make-bed --out unrelateds
+plink1.90 --bfile scratch/chr1 --merge-list mergefile.txt --make-bed --out data
 
 # Make fake IDs
-cp unrelateds.fam unrelateds.fam.orig
-nl unrelateds.fam.orig | awk '{ print "id"$1, "id"$1, "0", "0", $6, $7 }' > unrelateds.fam
+cp data.fam data.fam.orig
+nl data.fam.orig | awk '{ print "id"$1, "id"$1, "0", "0", $6, $7 }' > data.fam
+
+# Check sex
+plink1.90 --bfile data --check-sex --out data
